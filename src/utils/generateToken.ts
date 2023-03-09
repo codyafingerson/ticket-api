6469a7e7b9cd7cf2ced2afea85e5082cda96d4e9
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-08 22:54:20 -0700

    Add comment stating the functionality of the jwt.sign method

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
  * @returns jwt token
  */
 function generateToken(id: string) {
+  // sign the token with the user id and the secret
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
